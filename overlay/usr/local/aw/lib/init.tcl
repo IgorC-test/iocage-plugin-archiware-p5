bfe147afc66d47ceb6493731fdcf178058a60045
IgorC <igor.cigrovski@nobium.hr>
2020-07-29 12:35:46 +0200

    changed installation to tar unpack of tgz

--- overlay/usr/local/aw/lib/init.tcl
+++
@@ -1,1 +0,0 @@
-ns_moduleload lexx [file join [ns_info home] bin lexx[info sharedlib]]
--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-mkdir /usr/local/aw/log
+echo "unpacking tar"
+tar -xvf /usr/local/aw/awpst*_freebsd12.tgz -C /usr/local/aw/
+echo "deleting tgz"
+rm /usr/local/aw/awpst*_freebsd12.tgz
+echo "starting server"
 /usr/local/aw/start-server
 echo "Archiware P5 now installed" > /root/PLUGIN_INFO
